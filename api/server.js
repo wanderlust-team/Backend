11723622caf6a8361b5cadbef28a0f94713e2662
ddot727 <ddot727@gmail.com>
2019-04-15 16:49:11 -0400

    connected auth and users routers

--- api/server.js
+++ api/server.js
@@ -1,13 +1,17 @@
 const helmet = require('helmet');
 const express = require('express');
 
-
+const authRouter = require("../routers/auth/authRouter.js");
+const usersRouter = require("../routers/users/usersRouter.js");
 
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+server.use("/api/auth", authRouter);
+server.use("/api/users", usersRouter);
 
 server.get('/', async (req, res) => {
   res.status(200).json({ api: 'up' });
