8d3fedf201f0106a35b9ff1b23c52be6a8ec3219
Jor_Poon <jormpoon@gmail.com>
2019-04-15 17:25:31 -0400

    debuggin

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data/wandeLust.db3'
+      filename: './data/wanderLust.db3'
     },
     pool: {
       afterCreate: (conn, done) => {
