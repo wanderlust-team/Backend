e3e5588c5bc93b010c0232e50721caeb27458002
ddot727 <ddot727@gmail.com>
2019-04-16 15:04:13 -0400

    fixed migration...hopefully

--- data/migrations/20190415135145_users.js
+++ data/migrations/20190415135145_users.js
@@ -1,12 +1,11 @@
 
 exports.up = function (knex) {
   return knex.schema.createTable('users', users => {
-    users.increments('userId');
+    users.increments();
 
     users
       .string('username', 255)
-      .notNullable()
-      .unique();
+      .notNullable();
     users.string('password', 255).notNullable();
     users.string('email', 128).notNullable();
     users.string("userType", 128).notNullable();
