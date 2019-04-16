235fe46bd0b14c3a4683e88b3914b0d44b2eef82
ddot727 <ddot727@gmail.com>
2019-04-16 13:24:11 -0400

    set seed up/ moving on to testing it out

--- data/seeds/002-trips.js
+++ data/seeds/002-trips.js
@@ -1,13 +1,23 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+exports.seed = function (knex, Promise) {
+  // Deletes ALL existing entries and resets ids
+  return knex("trips")
+    .truncate()
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex("trips").insert([
+        {
+          tripName: "Hiking In The Jungle",
+          location: "Panama",
+          description: "Come join us on an amazing adventure through the Chagras jungle!",
+          startDate: 04 - 16 - 2019,
+          endDate: 04 - 18 - 2019
+        },
+        {
+          tripName: "Swim On The Nile",
+          location: "Egypt",
+          description: "Ever wanted to swim on the longest river? Now is your chance!",
+          startDate: 04 - 16 - 2019,
+          endDate: 04 - 18 - 2019
+        }
       ]);
     });
 };
