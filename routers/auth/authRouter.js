d3092e48171b0841f678d9f42240653699549593
Jor_Poon <jormpoon@gmail.com>
2019-04-17 16:38:50 -0400

    user.id through login endpoint

--- routers/auth/authRouter.js
+++ routers/auth/authRouter.js
@@ -34,7 +34,7 @@ router.post('/login', (req, res) => {
 
         res.status(200).json({
           message: `Welcome ${user.username}!`,
-          token,
+          token, user: user.id
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
